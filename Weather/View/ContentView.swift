501cfc78c3d9f0909d3a5094e378b5abc2666fce
Mark Davis <mark.j.davis2@gmail.com>
2023-11-15 15:58:47 -0500

    add launch screen and splash icon

--- Weather/View/ContentView.swift
+++ Weather/View/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                    .foregroundColor(Color("Text"))
+                    .font(.title)
+            }
+            .padding()
+            .background(Color("Background"))
         }
-        .padding()
+        .background(Color("Background"))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
