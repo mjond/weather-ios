3c98fc84a659dec2110a8bb66e48a31883f5b109
Mark Davis <mark.j.davis2@gmail.com>
2024-11-06 11:33:39 -0500

    save settings in user defaults

--- Weather/Scenes/Settings/Model/SettingsModel.swift
+++ Weather/Scenes/Settings/Model/SettingsModel.swift
@@ -8,28 +8,29 @@
 import SwiftUI
 
 final class WeatherSettings: ObservableObject {
-    @Published var unitOfMeasurement: UnitOfMeasurement = .imperial
-//    @Published var unitOfMeasurement: UnitOfMeasurement {
-//        didSet {
-//            updatePreference(value: unitOfMeasurement, key: UserDefaultsConstants.unitOfMeasurementSetting.rawValue)
-//        }
-//    }
-//
-//    init() {
-//        if let storedUnits = UserDefaults().value(forKey: UserDefaultsConstants.unitOfMeasurementSetting.rawValue) as? String {
-//            if storedUnits == UnitOfMeasurement.imperial.rawValue {
-//                unitOfMeasurement = .imperial
-//            } else {
-//                unitOfMeasurement = .metric
-//            }
-//        }
-//        // if there isn't a stored setting, then default to imperial
-//        unitOfMeasurement = .imperial
-//    }
-//    
-//    func updatePreference(value: UnitOfMeasurement, key: String) {
-//        UserDefaults().set(value.rawValue, forKey: key)
-//    }
+    @Published var unitOfMeasurement: UnitOfMeasurement {
+        didSet {
+            updatePreference(value: unitOfMeasurement, key: UserDefaultsConstants.unitOfMeasurementSetting.rawValue)
+            print("Set unit of measurement to: \(unitOfMeasurement)")
+        }
+    }
+
+    init() {
+        if let storedUnits = UserDefaults().value(forKey: UserDefaultsConstants.unitOfMeasurementSetting.rawValue) as? String {
+            if storedUnits == UnitOfMeasurement.imperial.rawValue {
+                unitOfMeasurement = .imperial
+            } else {
+                unitOfMeasurement = .metric
+            }
+        } else {
+            // if there isn't a stored setting, then default to imperial
+            unitOfMeasurement = .imperial            
+        }
+    }
+    
+    func updatePreference(value: UnitOfMeasurement, key: String) {
+        UserDefaults().set(value.rawValue, forKey: key)
+    }
 }
 
 enum UnitOfMeasurement: String {
